594f005646cd39dd88412c70befbd12aad649cc2
jhos_mf1 <jhostinmapura71@gmail.com>
2026-07-13 21:36:21 -0500

    feat(added objects formula car and grand prix): creation of two new objects (fomula car and grand prix), besides in grand prix was introduced the first model of a free practice simulation (just basic)

--- objects.py
+++ objects.py
@@ -1,4 +1,4 @@
-class Circuit():
+class Circuit(): #keeps importaant circuit's info
     def __init__(self, distance, numCorners, numStraights):
         self.distance = distance
         self.numCorners = numCorners
@@ -24,9 +24,211 @@ class Circuit():
 
             self.straightsDistance.append(float(straightDistance.strip()))
 
+class FormulaCar(): #keeps a formula's car info
+    def __init__(self, horsePower, maxTorque, tiresType):
+        self.horsePower = horsePower
+        self.maxTorque = maxTorque
+        self.tiresType = tiresType
+    
+class GrandPrix(): #receives a car and a circuit to start the grand prix 
+    def __init__(self, formulaCar, circuit, sprint):
+        self.formulaCar = formulaCar
+        self.circuit = circuit
+        self.sprint = sprint
+        
+    def freePractice(self): #simulation of free practice
 
-barcelona = Circuit(8,2,2)
-barcelona.cornersInfo()
-barcelona.straigthsInfo()
+        lapsNumber = int(input("Enter the number of laps that you're gonna test: "))
+        lapsRestore = lapsNumber
+        tyreWear = 0
+        
+        if self.sprint == False: # if there isn't sprint race
+            sesion = 1
+            print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
 
-print(barcelona.cornersDistance[0])
+            while lapsNumber > 0 and sesion <= 3 : # until there aren't laps and sesions remaining
+                print(f'\n------------Laps Remaining {lapsNumber}------------\n')
+
+                if self.formulaCar.tiresType == "Soft":
+                    tyreWear += 8
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Medium":
+                    tyreWear += 5
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Hard":
+                    tyreWear += 3
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Intermediate":
+                    tyreWear += 3
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Full Wet":
+                    tyreWear += 2
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                else:
+                    print('type of tire nor found')
+
+
+                if lapsNumber == 0 :#when there aren't laps remaining it changes to the nex sesion
+                    print(f'sesion number {sesion} finished, changing to the next one')
+                    sesion +=1
+                    lapsNumber = lapsRestore
+                    tyreWear = 0
+                    print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+
+        else:
+            sesion = 1
+
+            print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+
+            while lapsNumber > 0 and sesion <= 2 : # until there aren't laps and sesions remaining
+
+                print(f'\n------------Laps Remaining {lapsNumber}------------\n')
+
+                if self.formulaCar.tiresType == "Soft":
+                    tyreWear += 8
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Medium":
+                    tyreWear += 5
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Hard":
+                    tyreWear += 3
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Intermediate":
+                    tyreWear += 3
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                elif self.formulaCar.tiresType == "Full Wet":
+                    tyreWear += 2
+                    print(f'the current tyre wear is: {tyreWear}')
+
+                    if tyreWear > 100:# if tyre's wear is over 100% the car will be retired and pass to the next practice sesion
+                        print(f'retiring the car, since the current tyre wear is: {tyreWear} over the limit')
+                        print(f'sesion number {sesion} finished, changing to the next one')
+                        sesion +=1
+                        lapsNumber = lapsRestore
+                        tyreWear = 0
+                        print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                        continue
+                    lapsNumber -= 1
+
+                else:
+                    print('type of tire nor found')
+
+
+                if lapsNumber == 0 :#when there aren't laps remaining it changes to the nex sesion
+                    print(f'sesion number {sesion} finished, changing to the next one')
+                    sesion +=1
+                    lapsNumber = lapsRestore
+                    tyreWear = 0
+                    
+                    print(f'\n------------Starting with free practice simulation, sesion {sesion} ------------\n')
+                
+
+
+
+redBullRing = Circuit(5.2, 3, 2)
+redBullRing.cornersInfo()
+redBullRing.straigthsInfo()
+
+redBull = FormulaCar(850, 720, "Soft")
+
+gp_test1 = GrandPrix(redBull,redBullRing,False)
+gp_test1.freePractice()
